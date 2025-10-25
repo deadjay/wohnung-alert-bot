3ab0bc6c541bdddb65065f3b26028244d7f0c595
Artem Alekseev <iaffector@gmail.com>
2025-10-25 16:43:59 +0200

    Set update interval to 5 mins

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from datetime import datetime
 load_dotenv()
 TELEGRAM_BOT_TOKEN = os.getenv("telegram_bot_token")
 SUBSCRIBERS_FILE = "subscribers.json"
-LISTINGS_UPDATE_INTERVAL = 600 # 10 mins
+LISTINGS_UPDATE_INTERVAL = 300 # 5 mins
 REQUEST_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
